d4500982c2fe41c90f1f95093c253be854eaa882
“Owusu <owusuachamfour@gmail.com>
2023-09-19 06:10:43 -0400

    All my files created.

---
+++ server.js
@@ -0,0 +1,28 @@
+//dependencies
+const express = require("express");
+const app = express();
+const jsxEngine = require("jsx-view-engine");
+
+//models
+const pokemons = require("./models/pokemons.js");
+
+//adding view templates
+app.set("view engine", "jsx");
+app.engine("jsx", jsxEngine());
+
+//controllers implemented as routes
+//pokemon routes
+app.get("/pokemons/", (req, res) => {
+  res.render("Index", { pokemons: pokemons });
+});
+
+app.get("/pokemons/:indexOfPokemonsArray", (req, res) => {
+  res.render("Show", {
+    pokemon: pokemons[req.params.indexOfPokemonArray],
+  });
+});
+
+//listen on port 3000
+app.listen(3000, () => {
+  console.log("listening");
+});
