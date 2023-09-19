a2768c20c53d0ce4f098d0477e5e1cc3788b9a7b
“Owusu <owusuachamfour@gmail.com>
2023-09-19 08:16:21 -0400

    Added dynamic anchor tag comment to server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get("/pokemon/", (req, res) => {
   res.render("Index", { pokemons: pokemon });
 });
 
+//dynamic anchor tags added in Index.jsx
 app.get("/pokemon/:indexOfPokemonsArray", (req, res) => {
   res.send(req.params.indexOfPokemonsArray);
 });
